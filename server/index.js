afce04ac76e1347bee25497025ee25fb1c0cfdd2
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-06-15 19:00:08 +0530

    add: update index js

--- server/index.js
+++ server/index.js
@@ -1,22 +1,20 @@
-// server/index.js
 const express = require('express');
+const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-require('dotenv').config();
+const cors = require('cors');
 
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-// Connect MongoDB
+dotenv.config();
 connectDB();
 
-// Middleware to parse JSON
+const app = express();
+app.use(cors());
 app.use(express.json());
 
-// Sample route
-app.get('/', (req, res) => {
-  res.send('🚀 LankaRail Backend is Running...');
-});
+// Routes
+const authRoutes = require('./routes/authRoutes');
+app.use('/api/auth', authRoutes);
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`✅ Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
