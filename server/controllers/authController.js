ab016e424b21ad7d0f4e3bc47e7e7809f456330e
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-06-15 18:58:06 +0530

    feat: create authcontroll file

---
+++ server/controllers/authController.js
@@ -0,0 +1,32 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+exports.register = async (req, res) => {
+  const { fullName, email, password, role } = req.body;
+
+  if (!fullName || !email || !password || !role) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  try {
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(409).json({ message: "Email already registered" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+      role,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
+};
