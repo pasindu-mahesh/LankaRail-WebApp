0c0d5b1bb8ff35ceef3dae9a64985dbd39e0a5d3
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-06-15 18:56:47 +0530

    feat: create user js file and upadate dbjs file

--- server/config/db.js
+++ server/config/db.js
@@ -1,16 +1,16 @@
-// server/config/db.js
 const mongoose = require('mongoose');
-require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB Atlas Connected');
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected");
   } catch (error) {
-    console.error('MongoDB connection failed:', error.message);
-    process.exit(1); // Exit process if connection fails
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
   }
 };
 
 module.exports = connectDB;
-
