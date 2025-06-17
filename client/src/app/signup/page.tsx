75e44964df313291a2615c99f15424582c7bb8e7
Pasindu Mahesh Kannangara <158203795+pasindu-mahesh@users.noreply.github.com>
2025-06-17 23:21:31 +0530

    fix : updte sign up

--- client/src/app/signup/page.tsx
+++ client/src/app/signup/page.tsx
@@ -34,7 +34,7 @@ export default function SignupPage() {
           "Content-Type": "application/json"
         },
         body: JSON.stringify({
-          name,
+          name ,
           email,
           password,
           userType
